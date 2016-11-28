2555989f44d169814966eafe16e50db1a5571f5f
sbstnplcn <sebastien.palacin@gmail.com>
2016-11-28 15:17:14 +0100

    view all project by a student

--- public/src/js/components/student/student.js
+++ public/src/js/components/student/student.js
@@ -2,13 +2,24 @@
 
     app.component('student', {
         templateUrl: 'src/js/components/student/student.html',
-        controller: ['studentsService', '$stateParams', function(studentsService, $stateParams) {
+        controller: ['studentsService',
+                    '$stateParams',
+                    'projectsService', function(studentsService, $stateParams, projectsService) {
             angular.extend(this, {
                 $onInit() {
                     studentsService.getPopulate($stateParams).then((res) => {
                         this.student = res.data
-                        this.projects = this.student.projects
+                        this.studentProjects = this.student.projects
                     })
+                    projectsService.get().then((res) => {
+                        this.projects = res.data
+                    })
+
+                    this.addProject = (id, [newProject]) => {
+                        studentsService.edit(id, newProject).then((res)=>{
+
+                        })
+                    }
 
                 }
 
