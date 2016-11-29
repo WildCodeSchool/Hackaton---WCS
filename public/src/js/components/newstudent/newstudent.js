478688e5a1275dab2ee0146c7cbfac28e6bf81be
sbstnplcn <sebastien.palacin@gmail.com>
2016-11-29 12:15:25 +0100

    match project and student when i create a student

--- public/src/js/components/newstudent/newstudent.js
+++ public/src/js/components/newstudent/newstudent.js
@@ -2,16 +2,50 @@
     'use strict'
     app.component('newstudent', {
         templateUrl: '/src/js/components/newstudent/newstudent.html',
-        controller: ['studentsService', function(studentsService) {
+        controller: ['studentsService', 'projectsService', function(studentsService, projectsService) {
             angular.extend(this, {
                 $onInit() {
 
-                    this.add = () => {
-                        this.newStudent.avatar = this.newStudent.avatar.base64
-                        studentsService.add(this.newStudent).then((res) => {
-                            console.log(this.newStudent);
-                        })
+                    projectsService.get().then((res) => {
+                        this.projects = res.data
+                    })
+
+                    this.add = (newStudent, newProject) => {
+                        //encode avatar in base64
+                        newStudent.avatar = newStudent.avatar.base64
+                        for (let i in this.projects) {
+                            if (newProject == this.projects[i].title) {
+                                newStudent.projects = [this.projects[i]._id]
+                                //create new student
+                                return studentsService.add(this.newStudent).then((res) => {
+                                    //create allStudents + the new one
+                                    studentsService.get().then((res) => {
+                                        let allStudents = res.data
+                                        //je boucle dans tout les student
+                                        for (let j in allStudents) {
+                                            //je match le nom
+                                            if (newStudent.name == allStudents[j].name) {
+                                                //creer une variable project pour editer le this.projects
+                                                let project = {}
+                                                project['_id'] = this.projects[i]._id
+                                                project['student'] = allStudents[j]._id
+                                                //J'edite le project dans le select
+                                                return projectsService.edit(project).then((res) => {
+
+                                                })
+                                            }
+                                        }
+                                    })
+                                })
+                            }
+                        }
+                        this.newStudent = {}
                     }
+
+                    this.addProject = () => {
+
+                    }
+
                     this.cancel = () => {
                         this.newStudent = {}
                     }
