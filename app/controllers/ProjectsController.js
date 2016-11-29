15fb46797cdfe7e4d670082ff8606de4368ac33e
uxpaul <hoedevproject@gmail.com>
2016-11-29 07:00:29 +0100

    Add instant search with RegExp

--- app/controllers/ProjectsController.js
+++ app/controllers/ProjectsController.js
@@ -2,34 +2,88 @@
 
 let Controller = require('./Controller') // on étend la classe
 const PROJECT = require('../models/projects')
-
-// La classe BlogsController étend controller, on peut l'étendre encore et encore et encore...
+const STUDENTS = require('../models/students.js')
+    // La classe BlogsController étend controller, on peut l'étendre encore et encore et encore...
 
 class ProjectsController extends Controller {
-  constructor(){
-    super(PROJECT)
-  }
+    constructor() {
+        super(PROJECT)
+    }
 
-  find(req,res, next){
-    this.model.find(req.query).populate({
-      path:'student',
-      populate:{ path: 'projects'}
-    }).exec((err, documents)=>{
-      if(err) next(err)
-      else res.json(documents)
-    })
-  }
+    find(req, res, next) {
+        this.model.find(req.query).populate({
+            path: 'student',
+            populate: {
+                path: 'projects'
+            }
+        }).exec((err, documents) => {
+            if (err) next(err)
+            else res.json(documents)
+        })
+    }
+
+    findOne(req,res, next){
+      console.log(req.params.title)
+      let data =  new RegExp('^'+req.params.title, "i")
+      this.model.findOne({"title": data },(err,document)=>{
+        if (err) next(err)
+        else {
+          res.json(document)
+        }
+      })
+    }
 
 
-  findById(req,res, next){
-    this.model.findById(req.params.id).populate({
-      path:'student',
-      populate:{ path: 'projects'}
-    }).exec((err, document)=>{
-      if (err) next(err)
-      else res.json(document)
-    })
-  }
+    findById(req, res, next) {
+        this.model.findById(req.params.id).populate({
+            path: 'student',
+            populate: {
+                path: 'projects'
+            }
+        }).exec((err, document) => {
+            if (err) next(err)
+            else res.json(document)
+        })
+    }
+
+    create(req, res, next) {
+        let student = req.body.student
+        delete req.body.student
+        this.model.create(req.body, (err, project) => {
+            if (err) next(err)
+            else {
+
+                project.student = []
+                for (let i = 0; i < student.split(',').length; i++) {
+                    let element = student.split(',')[i]
+
+                    STUDENTS.findOne({
+                        name: element
+                    }, (err, document) => {
+                        if (!document) {
+
+                            console.log("*** CREATE " + element + "***")
+
+                            document = new STUDENTS()
+                            document.name = element
+                            document.save()
+
+                        }
+                        project.student.push(document.id)
+                        if (i == student.split(',').length - 1) {
+                            console.log(project.student)
+                            project.save()
+                        }
+
+                    })
+                }
+
+                res.json(project)
+
+            }
+        })
+    }
+
 
 }
 
