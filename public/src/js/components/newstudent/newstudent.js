2c467c356615b60fe0126aecc42484761173dc38
sbstnplcn <sebastien.palacin@gmail.com>
2016-11-29 22:36:19 +0100

    change css project page + change add student + add select

--- public/src/js/components/newstudent/newstudent.js
+++ public/src/js/components/newstudent/newstudent.js
@@ -13,32 +13,44 @@
                     this.add = (newStudent, newProject) => {
                         //encode avatar in base64
                         newStudent.avatar = newStudent.avatar.base64
-                        for (let i in this.projects) {
-                            if (newProject == this.projects[i].title) {
-                                newStudent.projects = [this.projects[i]._id]
-                                //create new student
-                                return studentsService.add(this.newStudent).then((res) => {
-                                    //create allStudents + the new one
-                                    studentsService.get().then((res) => {
-                                        let allStudents = res.data
-                                        //je boucle dans tout les student
-                                        for (let j in allStudents) {
-                                            //je match le nom
-                                            if (newStudent.name == allStudents[j].name) {
-                                                //creer une variable project pour editer le this.projects
-                                                let project = {}
-                                                project['_id'] = this.projects[i]._id
-                                                project['student'] = allStudents[j]._id
-                                                //J'edite le project dans le select
-                                                return projectsService.edit(project).then((res) => {
-
-                                                })
+                        if (newProject[0] = undefined) {
+                            for (let i in this.projects) {
+                                if (newProject == this.projects[i].title) {
+                                    newStudent.projects = [this.projects[i]._id]
+                                        //create new student
+                                    return studentsService.add(this.newStudent).then((res) => {
+                                        //create allStudents + the new one
+                                        studentsService.get().then((res) => {
+                                            let allStudents = res.data
+                                                //je boucle dans tout les student
+                                            for (let j in allStudents) {
+                                                //je match le nom
+                                                if (newStudent.name == allStudents[j].name) {
+                                                    //creer une variable project pour editer le this.projects
+                                                    let project = {}
+                                                    project['_id'] = this.projects[i]._id
+                                                    project['student'] = allStudents[j]._id
+                                                        //J'edite le project dans le select
+                                                    return projectsService.edit(project).then((res) => {
+                                                        console.log('project');
+                                                    })
+                                                }
                                             }
-                                        }
+                                        })
                                     })
-                                })
+                                }
+                                else {
+                                    studentsService.add(this.newStudent).then((res) => {
+                                        console.log('no-project');
+                                    })
+                                }
                             }
+                        } else {
+                            studentsService.add(this.newStudent).then((res) => {
+                                console.log('no-project');
+                            })
                         }
+
                         this.newStudent = {}
                     }
 
