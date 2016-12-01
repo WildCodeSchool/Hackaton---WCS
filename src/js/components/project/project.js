2338c2189c7f6a2ce1d71a03b582dfa099ff19de
uxpaul <hoedevproject@gmail.com>
2016-12-01 09:51:28 +0100

    Add animation slider

--- src/js/components/project/project.js
+++ src/js/components/project/project.js
@@ -35,8 +35,8 @@
                         timer = $timeout(() => {
                             this.nexte();
                             this.suivant();
-                            timer = $timeout(sliderFunc, 3000);
-                        }, 3000);
+                            timer = $timeout(sliderFunc, 5000);
+                        }, 5000);
                     };
 
                     sliderFunc();
