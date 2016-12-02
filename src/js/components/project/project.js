cf80eed68a0723492b2a360b8d334ee7e7f6080f
uxpaul <hoedevproject@gmail.com>
2016-12-02 10:38:56 +0100

    Factorize

--- src/js/components/project/project.js
+++ src/js/components/project/project.js
@@ -16,17 +16,14 @@
                     this.begin = 0;
                     this.editMode
 
-                    commentsService.get().then((res) => {
-                        this.comments = res.data
-                    })
-
+                    
                     // add new comment
                     this.addComment = (comment, project) => {
                         this.comment.projects = project._id
                         commentsService.add(this.comment).then((res) => {
                             this.test = res.data._id
                             this.comment = ""
-                            // call project() and pass parameters
+                                // call project() and pass parameters
                             this.project(this.test, project);
                         })
 
@@ -60,7 +57,7 @@
                         if (this.editMode) {
                             this.comment = project.comments[index]
                             commentsService.edit(this.comment).then((res) => {
-                              console.log(res.data)
+                                console.log(res.data)
                             })
                             projectsService.edit(project).then((res) => {
                                 this.projects = res.config.data
