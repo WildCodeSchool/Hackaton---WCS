b0bd7418ecb3a8be9e6dc5fac4297b26ba77776d
sbstnplcn <sebastien.palacin@gmail.com>
2016-12-05 08:46:42 +0100

    bidirectionnal edit user and project when add a new project

--- src/js/components/newproject/newproject.js
+++ src/js/components/newproject/newproject.js
@@ -3,27 +3,59 @@
     app.component('newproject', {
         templateUrl: 'js/components/newproject/newproject.html',
         controller: ['projectsService', "studentsService", function(projectsService, studentsService) {
-            // let student = _.find(this.students, (student) => {
-            //     return(student.name === this.project.student)
-            // })
-            //if (student)
-            //this.project.student = student._id
-            this.date = new Date();
+
+            this.date = new Date()
+
             this.projects = []
 
             studentsService.get().then((res) => {
-                    this.students = res.data
-                })
-                //Add a new Project
+                this.students = res.data
+            })
+
+            //Add a new Project
             this.add = () => {
                 console.log(this.project)
-                projectsService.add(this.project).then((res) => {
-                    this.projects.push(res.data)
-                    this.project = {}
-                })
-
+                //check if student selected
+                if (this.project.student != undefined){
+                    //loop in all students
+                    for (let i in this.students){
+                        //check if this.project.student match with one of student name
+                        if (this.project.student == this.students[i].name){
+                            //catch id of this student
+                            //this.project.student = this.students[i]._id
+                            //push new project with student id
+                            return projectsService.add(this.project).then((res) => {
+                                this.projects.push(res.data)
+                                //get all new projects
+                                projectsService.get().then((res)=>{
+                                    let allNewProjects = res.data
+                                    //loop in all new project
+                                    for (let j in allNewProjects){
+                                        //catch id of this.project
+                                        if (this.project.title == allNewProjects[j].title){
+                                          //create newstudent
+                                          let newStudent = {}
+                                          newStudent['_id'] = this.students[i]._id
+                                          newStudent['projects'] = allNewProjects[j]._id
+                                          //edit newstudent with its new project
+                                          return studentsService.edit(newStudent).then((res)=>{
+                                              this.project = {}
+                                          })
+                                        }
+                                    }
+                                })
+                            })
+                        }
+                    }
+                } else {
+                    this.project.student = ""
+                    projectsService.add(this.project).then((res) => {
+                        this.projects.push(res.data)
+                        this.project = {}
+                    })
+                }
+                this.project = {}
             }
-
 
             // Cancel
             this.annuler = (project) => {
